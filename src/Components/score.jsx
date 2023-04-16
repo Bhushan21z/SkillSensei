c6471a9be1c56479f016f00125d0546a6256270f
Bhushan-Wanjari <118363675+Bhushan-Wanjari@users.noreply.github.com>
2023-04-17 02:49:58 +0530

    chart added

--- src/Components/score.jsx
+++ src/Components/score.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { Grid, Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material';
+import RadialBar from "../Pages/Radial/RadialBar";
+import { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+// import { Doughnut } from 'react-chartjs-2';
 
 
 
@@ -24,7 +29,21 @@ const styles = {
 
 
 
-const score = () => {
+const score = (props) => {
+  const email= props.email
+  const data= props.data
+  const timedata=[data.mintime, data.time, data.avgtime, data.maxtime];
+  const attemptdata=[data.minattempt, data.attempt, data.avgattempt, data.maxattempt];
+  const label=["Minimum time", "Your Time", "Average Time", "Maximum Time"];
+  const label2=["Minimum Attempts", "Your Attempts", "Average Attempts", "Maximum Attempts"];
+  // const series=[100, 255, 41, 17, 15];
+  let levelstr="You are on Level"+data.level+" Complete Game to see your Rank";
+  if(data.level==6){
+    levelstr=" Your Rank is #"+data.rank;
+  }
+  console.log(levelstr);
+  console.log(timedata);
+  
   return (
    <div>
    <Grid container sx={{
@@ -42,8 +61,9 @@ const score = () => {
         color: '#fff',
         fontSize: '2rem',
         fontWeight: 'bold',
+        mt: '150px',
      }} >
-      ScoreBoard
+      {levelstr}
      </Typography>
     </Grid>
 
@@ -60,32 +80,108 @@ const score = () => {
         >
   {/* 1st item */}
   <Grid item sx={styles.features}>
-         
-          <h4 >Increase Security</h4>
+          <h4>Increase Security</h4>
           <Typography variant="body1" sx={styles.text}>
-          Score
+          Time Taken
           </Typography>
+          <RadialBar label={label} timedata={timedata} />
 
-          {/* chart */}
-            <div> 
-            <canvas id="myChart" width="400" height="400">
-
-
-            </canvas>
-            </div>
 
         </Grid>
 
         <Grid item sx={styles.features}>
-         
-          <h4 >Increase Security</h4>
           <Typography variant="body1" sx={styles.text}>
           Attempts
           </Typography>
+          <RadialBar label={label2} timedata={attemptdata} />
+
+   
         </Grid>
-
-
 </Grid>
+
+{/* -------------------------------Stats------------------------------------- */}
+
+{/* "allData": {
+		"email": "gg@gmail.com",
+		"rank": 4,
+		"time": 541299,
+		"attempt": 0,
+		"mintime": 50,
+		"minattempt": 0,
+		"maxtime": 79080450,
+		"maxattempt": 14,
+		"avgtime": 13653304,
+		"avgattempt": 4,
+		"level": 6,
+		"game1": [
+			1,
+			0,
+			43715
+		],
+		"game2": [
+			1,
+			0,
+			15754
+		],
+		"game3": [
+			1,
+			0,
+			75090
+		],
+		"game4": [
+			1,
+			0,
+			63459
+		],
+		"game5": [
+			1,
+			0,
+			343281
+		]
+	} */}
+
+<Grid item xs={10} sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          gap: "2rem",
+        }}
+        >
+
+
+<Grid item sx={styles.features}>
+          
+          <Typography variant="body1" sx={styles.text}>
+        Rank
+          </Typography>
+
+          </Grid>
+
+          <Grid item sx={styles.features}>
+         
+          <Typography variant="body1" sx={styles.text}>
+          Attempts
+          </Typography>
+
+          </Grid>
+          <Grid item sx={styles.features}>
+         
+          <Typography variant="body1" sx={styles.text}>
+          Attempts
+          </Typography>
+
+          </Grid>
+          <Grid item sx={styles.features}>
+         
+          <Typography variant="body1" sx={styles.text}>
+          Attempts
+          </Typography>
+
+          </Grid>
+
+
+          </Grid>
 
 
 </Grid>
@@ -95,4 +191,4 @@ const score = () => {
   )
 }
 
-export default score
+export default score;
